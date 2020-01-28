1f94030469f230aa72f2ed54caf452a88a562b71
ekloberdanz <kloberdanz.eliska@gmail.com>
2020-01-28 15:00:20 -0600

    Add files via upload

--- substitute_resnet_50_keras_cifar.py
+++ substitute_resnet_50_keras_cifar.py
@@ -57,7 +57,9 @@ from keras import optimizers
 from keras.models import Sequential, Model as Model_Keras,load_model
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D,GlobalAveragePooling2D
 from keras.callbacks import TensorBoard,ReduceLROnPlateau,ModelCheckpoint
-
+from keras.layers import Dense, Flatten
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import SGD, Adam
 
 FLAGS = flags.FLAGS
 NB_CLASSES = 100
@@ -71,58 +73,54 @@ LMBDA = .1
 AUG_BATCH_SIZE = 512
 
 
-def setup_tutorial():
-  """
-  Helper function to check correct configuration of tf for tutorial
-  :return: True if setup checks completed
-  """
 
-  # Set TF random seed to improve reproducibility
-  tf.set_random_seed(1234)
-  return True
+def train_oracle(x_train, y_train, x_test, y_test, img_rows, img_cols, nchannels, nb_classes):
 
 
-def prep_bbox():
 
-  (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
-
-  x_train = x_train/255.
-  x_test = x_test/255.
-  # Encode training labels to 1-of-c output
-  y_train = keras.utils.to_categorical(y_train,num_classes=100)
-  y_test = keras.utils.to_categorical(y_test,num_classes=100)
-  from keras.layers import Dense, Flatten
-  from keras.layers.pooling import MaxPooling2D
   model = Sequential()
-  #First Hidden Layer
-  model.add(Conv2D(16, (3, 3), activation='relu',  input_shape=(32,32,3)))
-  model.add(Conv2D(32, (3, 3), padding ='same', activation = 'relu',input_shape= (32, 32, 3)))
+  model.add(Conv2D(16, (3, 3), activation='relu',  input_shape=(img_rows,img_cols,nchannels)))
+  model.add(Conv2D(32, (3, 3), padding ='same', activation = 'relu',input_shape= (img_rows, img_cols, nchannels)))
   model.add(MaxPooling2D(2, 2))
   model.add(Flatten())
-  #Output Layer (10 digits => 10 neurons)
-  model.add(Dense(100, activation='softmax', kernel_initializer='random_normal'))
-
-
+  model.add(Dense(nb_classes, activation='softmax', kernel_initializer='random_normal'))  #Output Layer
+  
   '''
-  x_train = x_train[:1000]
-  y_train = y_train[:1000]
+  model = Sequential()
+  model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+  model.add(MaxPooling2D((2, 2)))
+  model.add(Conv2D(64, (3, 3), activation='relu'))
+  model.add(MaxPooling2D((2, 2)))
+  model.add(Conv2D(64, (3, 3), activation='relu'))
+  model.add(Flatten())
+  model.add(Dense(64, activation='relu'))
+  model.add(Dense(100, activation='softmax'))
+
+  #x_train = x_train[:1000]
+  #y_train = y_train[:1000]
   base_model = applications.resnet50.ResNet50(weights= None, include_top=False, input_shape= (32,32,3))
 
   x = base_model.output
   x = GlobalAveragePooling2D()(x)
   x = Dropout(0.5)(x)
-  predictions = Dense(NB_CLASSES, activation= 'softmax')(x)
+  predictions = Dense(100, activation= 'softmax')(x)
   model = Model_Keras(inputs = base_model.input, outputs = predictions)
+  
+
   '''
-  from keras.optimizers import SGD, Adam
-  # sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+
   adam = Adam(lr=0.001)
   model.compile(optimizer= adam, loss='categorical_crossentropy', metrics=['accuracy'])
-  model.fit(x_train, y_train, validation_split=0.33, epochs = 10, batch_size = 32, shuffle=True)
+  model.fit(x_train, y_train, validation_split=0.33, epochs = 1, batch_size = 50, shuffle=True)
 
-  preds = model.evaluate(x_test, y_test)
-  print ("Loss = " + str(preds[0]))
-  print ("Test Accuracy = " + str(preds[1]))
+  train_accuracy = model.evaluate(x_train, y_train)
+  print ("Loss = " + str(train_accuracy[0]))
+  print ("Train Accuracy = " + str(train_accuracy[1]))
+
+  test_accuracy = model.evaluate(x_test, y_test)
+  print ("Loss = " + str(test_accuracy[0]))
+  print ("Test Accuracy = " + str(test_accuracy[1]))
+
   model.summary()
   return model
 
@@ -138,7 +136,10 @@ class ModelSubstitute(Model):
     my_dense = functools.partial(
         tf.layers.dense, kernel_initializer=HeReLuNormalInitializer)
     with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-      y = tf.layers.flatten(x)
+      #y = tf.layers.flatten(x)
+      y = tf.layers.Conv2D(filters = 64, kernel_size = (5, 5), activation='relu')(x)
+      y = tf.layers.MaxPooling2D(pool_size=(2, 2), strides =(1, 1), padding='same')(y)
+      y = tf.layers.flatten(y)
       y = my_dense(y, self.nb_filters, activation=tf.nn.relu)
       y = my_dense(y, self.nb_filters, activation=tf.nn.relu)
       logits = my_dense(y, self.nb_classes)
@@ -168,7 +169,6 @@ def train_sub(sess, x, y, black_box_model, x_sub, y_sub, nb_classes,
   :param rng: numpy.random.RandomState instance
   :return:
   """
-  #saver = tf.train.Saver()
 
   # Define TF model graph (for the black-box model)
   model_sub = ModelSubstitute('model_s', nb_classes)
@@ -179,10 +179,6 @@ def train_sub(sess, x, y, black_box_model, x_sub, y_sub, nb_classes,
 
   # Define the Jacobian symbolically using TensorFlow
   grads = jacobian_graph(preds_sub, x, nb_classes)
-  print('\nDebugging')
-  print(preds_sub)
-  print(grads)
-  print(x_sub.shape)
 
   # Train the substitute and augment dataset alternatively
   for rho in xrange(data_aug):
@@ -205,37 +201,24 @@ def train_sub(sess, x, y, black_box_model, x_sub, y_sub, nb_classes,
       x_sub = jacobian_augmentation(sess, x, x_sub, y_sub, grads, lmbda_coef * lmbda, aug_batch_size)
 
       print("Labeling substitute training data.")
-      # Label the newly generated synthetic points using the black-box
+      # Label the newly generated synthetic points by quering the oracle
       y_sub = np.hstack([y_sub, y_sub])
       x_sub_prev = x_sub[int(len(x_sub)/2):]
-      eval_params = {'batch_size': batch_size}
-      #black_box_model.predict(x_sub_prev)
-      #bbox_val = batch_eval(sess, [x], [bbox_preds], [x_sub_prev],args=eval_params)[0]
-      # Note here that we take the argmax because the adversary
-      # only has access to the label (not the probabilities) output
-      # by the black-box model
-      print("Debugging")
-      print(y_sub)
-      print(len(x_sub))
-      print(x_sub)
-      print(y_sub[int(len(x_sub)/2):])
-      #y_sub[int(len(x_sub)/2):] = np.argmax(bbox_val, axis=1)
       bbox_preds = black_box_model.predict(x_sub_prev, batch_size=batch_size)
-      print(bbox_preds)
       bbox_labels = bbox_preds.argmax(axis=-1)
       print(bbox_labels)
-      y_sub[int(len(x_sub)/2):] = bbox_labels
+      y_sub[int(len(x_sub)/2):] = bbox_labels # the oracle only gives labels, not probabilities
   return model_sub, preds_sub
 
 
-def cifar10_blackbox(train_start=0, train_end=60000, test_start=0,
+def cifar100_blackbox(train_start=0, train_end=60000, test_start=0,
                    test_end=10000, nb_classes=NB_CLASSES,
                    batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE,
                    nb_epochs=NB_EPOCHS, holdout=HOLDOUT, data_aug=DATA_AUG,
                    nb_epochs_s=NB_EPOCHS_S, lmbda=LMBDA,
                    aug_batch_size=AUG_BATCH_SIZE):
   """
-  CIFAR10 tutorial for the black-box attack from arxiv.org/abs/1602.02697
+  arxiv.org/abs/1602.02697
   :param train_start: index of first training set example
   :param train_end: index of last training set example
   :param test_start: index of first test set example
@@ -250,12 +233,6 @@ def cifar10_blackbox(train_start=0, train_end=60000, test_start=0,
   # Set logging level to see debug information
   set_log_level(logging.DEBUG)
 
-  # Dictionary used to keep track and return key accuracies
-  accuracies = {}
-
-  # Perform tutorial setup
-  #assert setup_tutorial()
-
   # Create TF session
   config = tf.ConfigProto()
   config.gpu_options.per_process_gpu_memory_fraction = 0.4
@@ -268,17 +245,13 @@ def cifar10_blackbox(train_start=0, train_end=60000, test_start=0,
   print(x_train.shape[0], 'train samples')
   print(x_test.shape[0], 'test samples')
 
-  num_samples=len(x_train)
-
   # input image dimensions
-  nchannels , img_rows, img_cols = x_train.shape[3], x_train.shape[1], x_train.shape[2]
-  #x_train = np.moveaxis(x_train, 1, -1)
-  #x_test = np.moveaxis(x_test, 1, -1)
-  print('x_train shape:', x_train.shape)
+  img_rows, img_cols, nchannels = x_train.shape[1], x_train.shape[2], x_train.shape[3]
 
   x_train = x_train.astype('float32')
   x_test = x_test.astype('float32')
   
+  # Normalize
   x_train /= 255.
   x_test /= 255.
 
@@ -293,36 +266,27 @@ def cifar10_blackbox(train_start=0, train_end=60000, test_start=0,
   print ("X_test shape: " + str(x_test.shape))
   print ("Y_test shape: " + str(y_test.shape))
 
-  # Initialize substitute training set reserved for adversary
+  # Initialize substitute training set reserved for training substitute model
   x_sub = x_test[:holdout]
-  print('\nLABELS')
-  print(y_test[:holdout])
   y_sub = np.argmax(y_test[:holdout], axis=1)
-  print(y_sub)
-  print(y_train)
 
-
-  # Redefine test set as remaining samples unavailable to adversaries
+  # Redefine test set as remaining samples available only for training oracle
   x_test = x_test[holdout:]
   y_test = y_test[holdout:]
 
-  # Define input TF placeholder
-  x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols, nchannels))
-                                  
+  # Define input TF placeholders
+  x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols, nchannels))              
   y = tf.placeholder(tf.float32, shape=(None, nb_classes))
 
-  # Seed random number generator so tutorial is reproducible
+  # Seed random number generator for reproducible results
   rng = np.random.RandomState([2017, 8, 30])
 
-  # Simulate the black-box model locally
-  print("Preparing the black-box model.")
+  # Simulate the oracle model locally
+  print("Training the oracle")
+  black_box_model = train_oracle(x_train, y_train, x_test, y_test, img_rows, img_cols, nchannels, nb_classes)
+  print("Oracle ready")
 
-  black_box_model = prep_bbox()
-
-  print("\n predictions")
-  #print(bbox_preds)
-  #print("Accuracy of black box model", accuracy)
-  # Prepare for saving model
+  # Prepare for saving trained substitute model
   saver = tf.train.Saver()
 
   # Train substitute using method from https://arxiv.org/abs/1602.02697
@@ -348,11 +312,9 @@ def cifar10_blackbox(train_start=0, train_end=60000, test_start=0,
   save_path = saver.save(sess, "./substitute_model_RESNET50.ckpt")
   print("Model saved in path: %s" % save_path)
 
-  return accuracies
-
 
 def main(argv=None):
-  cifar10_blackbox(nb_classes=FLAGS.nb_classes, batch_size=FLAGS.batch_size,
+  cifar100_blackbox(nb_classes=FLAGS.nb_classes, batch_size=FLAGS.batch_size,
                  learning_rate=FLAGS.learning_rate,
                  nb_epochs=FLAGS.nb_epochs, holdout=FLAGS.holdout,
                  data_aug=FLAGS.data_aug, nb_epochs_s=FLAGS.nb_epochs_s,
